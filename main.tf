6f2524e9383244e46722872a0105dd2bd9cd118b
Ubuntu <ubuntu@ip-10-10-1-218.ap-northeast-1.compute.internal>
2020-02-04 06:19:45 +0000

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,77 @@
 terraform {
-	required_version = "~> 0.12"
+  required_version = "~> 0.12" 
+}
+
+provider "azurerm" {
+  client_id = var.client_id
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  client_secret = var.client_secret
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "my-vm-${count.index}-ryoma"
+  count = var.hello_tf_instance_count
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.*.id[count.index]]
+  vm_size               = var.vm_size
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "vmadmin"
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "my-osdisk-${count.index}-ryoma"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-groupi-ryoma"
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-network-ryoma"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet-ryoma"
+  resource_group_name   = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+yesource "azurerm_network_interface" "example" {
+  name                = "my-nw-interface-${count.index}-ryoma"
+  count = var.hello_tf_instance_count
+  location            = var.location
+  resource_group_name   = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "my-ip-config-ryoma"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "payground"
+  }
 }
